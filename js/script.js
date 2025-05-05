23d331840b4621abf0222b3e5d8fedb8a922b455
MIFTAH UL HAQ <miftah4165@gmail.com>
2025-05-06 00:54:07 +0600

    Added Screen one info

--- js/script.js
+++ js/script.js
@@ -1,1 +1,34 @@
+// function math(x,y) {
+//     let result= x+y
+//     console.log(result);
+// }
+// math(6,39)
+// math(45,9)
+// math(6,9)
 
+// 3 types of function=========================================
+
+// General Function
+function info () {    console.log("This is a function");
+
+}
+
+// Nameless Function
+let x = function(){
+    console.log("This is a nameless function");
+    
+}
+
+// Arrow Function
+let y =  () => {
+console.log("This is a arrowe function");
+
+}
+
+
+let members= ["A","V","Y","T"]
+
+members.map(info=>{
+    console.log(info);
+    
+})
